1422e5f9722586a8ee77fe8248e0948c02b45a53
Alejandro Alberto Gavira García <alex.gavira@gmail.com>
2024-02-09 10:52:28 +0100

    Initial Commit

---
+++ VisionOSTest/VisionOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSTestApp.swift
+//  VisionOSTest
+//
+//  Created by Alejandro Alberto Gavira García on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
